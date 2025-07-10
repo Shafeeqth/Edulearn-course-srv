f0e62816430dd479bbd0f633c9d226a86749a5b2
shafeeque <shafeeqsha06@gmail.com>
2025-07-10 22:39:36 +0530

    feat(lesson): add lesson module

---
+++ src/application/use-cases/lesson/lesson.module.ts
@@ -0,0 +1,28 @@
+import { Module } from "@nestjs/common";
+import { DatabaseRepositoryModule } from "src/infrastructure/database/database-repository.module";
+import { RedisModule } from "src/infrastructure/redis/redis.module";
+import { DeleteLessonUseCase } from "./delete-lesson.use-case";
+import { UpdateLessonUseCase } from "./update-leson.use-case";
+import { GetLessonsBySectionUseCase } from "./get-lessons-by-sections.use-case";
+import { CreateLessonUseCase } from "./create-lesson.use-case";
+import { KafkaModule } from "src/infrastructure/kafka/kafka.module";
+import { GetLessonUseCase } from "./get-lesson.use-case";
+
+@Module({
+  imports: [DatabaseRepositoryModule, RedisModule, KafkaModule],
+  providers: [
+    CreateLessonUseCase,
+    GetLessonsBySectionUseCase,
+    GetLessonUseCase,
+    UpdateLessonUseCase,
+    DeleteLessonUseCase,
+  ],
+  exports: [
+    GetLessonUseCase,
+    CreateLessonUseCase,
+    GetLessonsBySectionUseCase,
+    UpdateLessonUseCase,
+    DeleteLessonUseCase,
+  ],
+})
+export class LessonModule {}
