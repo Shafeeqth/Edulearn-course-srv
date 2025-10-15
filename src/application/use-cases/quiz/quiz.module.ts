2fbea21b4297e0328b819687c93cf6350e333cce
shafeeque <shafeeqsha06@gmail.com>
2025-10-15 23:40:35 +0530

    feat(quiz): add quiz module

---
+++ src/application/use-cases/quiz/quiz.module.ts
@@ -0,0 +1,28 @@
+import { Module } from "@nestjs/common";
+import { DatabaseRepositoryModule } from "src/infrastructure/database/database-repository.module";
+import { RedisModule } from "src/infrastructure/redis/redis.module";
+import { DeleteQuizUseCase } from "./delete-quiz.use-case";
+import { UpdateQuizUseCase } from "./update-quiz.use-case";
+import { GetQuizzesByCourseUseCase } from "./get-quizes-by-course.use-case";
+import { GetQuizUseCase } from "./get-quiz.use-case";
+import { CreateQuizUseCase } from "./create-quiz.use-case";
+import { KafkaModule } from "src/infrastructure/kafka/kafka.module"
+
+@Module({
+  imports: [DatabaseRepositoryModule, RedisModule, KafkaModule],
+  providers: [
+     CreateQuizUseCase,
+    GetQuizUseCase,
+    GetQuizzesByCourseUseCase,
+    UpdateQuizUseCase,
+    DeleteQuizUseCase,
+  ],
+  exports: [
+    CreateQuizUseCase,
+    GetQuizUseCase,
+    GetQuizzesByCourseUseCase,
+    UpdateQuizUseCase,
+    DeleteQuizUseCase,
+  ],
+})
+export class QuizModule {}
